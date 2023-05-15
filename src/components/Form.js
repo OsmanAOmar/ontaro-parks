d0eb5c01dac47f16e7631bf8ec5de87a982933ef
Osman Omar <o.ali.omar@gmail.com>
2023-05-14 20:07:45 -0400

    working on filtering parks based on selected activities

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,29 +3,57 @@ import firebase from "../firebase";
 import { getDatabase, ref, push, set } from "firebase/database";
 
 const Form = () => {
-    const [ radioValue, setRadioValue ] = useState("");
 
-    useState(() => {
-        setRadioValue("hiking");
-    }, [])
+    // Create a state variable to store selected activites. Initialize as an empty array
+    const [selectedActivities, setSelectedActivities] = useState([]);
 
-    const changeSelection = (e) => {
-        setRadioValue(e.target.value);
-    }
+
+    // Update the state when an activity is selected or deselected
+    // Attach an "onChange" event handler to each radio button input
+    const handleActivityChange = (event) => {
+        const { value } = event.target;
+        const updatedActivities = [...selectedActivities];
+      
+        if (updatedActivities.includes(value)) {
+          // Activity is already selected, so remove it
+          const index = updatedActivities.indexOf(value);
+          updatedActivities.splice(index, 1);
+        } else {
+          // Activity is not selected, so add it
+          updatedActivities.push(value);
+        }
+      
+        setSelectedActivities(updatedActivities);
+      };
+      
+      const filteredParks = parklist.filter((park) => {
+        return selectedActivities.every((activity) => park.activities[activity]);
+      });
+      
+
+
+
 
     return (
         <div>
             <h2>Choose your park activities!</h2>
             <form action="">
                 <label htmlFor="park" className="sr-only">Hiking</label>
-                <input type="radio" id="hiking" name="orderType" value="hiking" required onChange={changeSelection} checked={radioValue === 'hiking'}/>
+                <input type="radio" id="hiking" name="orderType" value="hiking" required onChange={handleActivityChange}/>
                 
                 <label htmlFor="park" className="sr-only">Swimming</label>
-                <input type="radio" id="swimming" name="orderType" value="swimming" required onChange={changeSelection} checked={radioValue === 'swimming'}
+                <input type="radio" id="swimming" name="orderType" value="swimming" required onChange={handleActivityChange} 
 
                 />
 
             </form>
+
+            <ul>
+  {filteredParks.map((park) => (
+    <li key={park.id}>{park.name}</li>
+  ))}
+</ul>
+
         </div>
     )
 }
--- src/components/Parklist.js
+++ src/components/Parklist.js
@@ -30,8 +30,11 @@ const Parklist = () => {
 
         const parkObj = {
           id: key,
-          title: dbObj[key].park.name
+          title: dbObj[key].park.name,
+          activity: dbObj[key].activities
         }
+
+
 
         console.log(parkObj)
 
@@ -44,8 +47,58 @@ const Parklist = () => {
     });
   }, []);
 
+  
+    // Create a state variable to store selected activites. Initialize as an empty array
+    const [selectedActivities, setSelectedActivities] = useState([]);
+
+
+    // Update the state when an activity is selected or deselected
+    // Attach an "onChange" event handler to each radio button input
+    const handleActivityChange = (event) => {
+        const { value } = event.target;
+        const updatedActivities = [...selectedActivities];
+      
+        if (updatedActivities.includes(value)) {
+          // Activity is already selected, so remove it
+          const index = updatedActivities.indexOf(value);
+          updatedActivities.splice(index, 1);
+        } else {
+          // Activity is not selected, so add it
+          updatedActivities.push(value);
+        }
+      
+        setSelectedActivities(updatedActivities);
+      };
+      
+      const filteredParks = parklist.filter((park) => {
+        return selectedActivities.every((activity) => park.activities[activity]);
+      });
+
   return (
     <section>
+
+<div>
+            <h2>Choose your park activities!</h2>
+            <form action="">
+                <label htmlFor="park" className="sr-only">Hiking</label>
+                <input type="radio" id="hiking" name="orderType" value="hiking" required onChange={handleActivityChange} />
+                
+                <label htmlFor="park" className="sr-only">Swimming</label>
+                <input type="radio" id="swimming" name="orderType" value="swimming" required onChange={handleActivityChange} 
+
+                />
+
+            </form>
+
+            <ul>
+  {filteredParks.map((park) => (
+    <li key={park.id}>{park.name}</li>
+  ))}
+</ul>
+
+        </div>
+
+
         <h2>Gorgeous list of parks</h2>
         <ul className="parklist">
             {
